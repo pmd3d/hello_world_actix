90783a2c4955ee573239accb567014687cfea3f7
Jon Lim <jjrr287@gmail.com>
2023-07-08 21:29:39 -0700

    initial draft of goal

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use actix_web::{web, guard::{self}, App, HttpServer, Responder, HttpRequest, get };
+
+pub struct greet;
+impl ::actix_web::dev::HttpServiceFactory for greet {
+    fn register(self, __config: &mut actix_web::dev::AppService) {
+        async fn greet(req: HttpRequest) -> impl Responder {
+            let name = req.match_info().get("name").unwrap_or("World");
+            format!("Hello, {}!", &name)
+        }
+        let __resource = ::actix_web::Resource::new("/")
+            .name("greet")
+            .guard(::actix_web::guard::Get())
+            .to(greet);
+        ::actix_web::dev::HttpServiceFactory::register(__resource, __config);
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)        
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+// #[get("/")]
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello, {}!", &name)
+// }
+//
+// cargo install cargo-expand
+// cargo +nightly expand
+
