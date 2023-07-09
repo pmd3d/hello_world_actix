1bf45b6e5a112b919f3eab146903972cec97693e
Jon Lim <jjrr287@gmail.com>
2023-07-08 23:35:21 -0700

    initial work of calling a new handler macro

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use actix_web::{web, guard::{self}, App, HttpServer, Responder, HttpRequest, get };
+use actix_web::{ App, HttpServer, Responder, HttpRequest, handler };
 
-pub struct greet;
-impl ::actix_web::dev::HttpServiceFactory for greet {
-    fn register(self, __config: &mut actix_web::dev::AppService) {
-        async fn greet(req: HttpRequest) -> impl Responder {
-            let name = req.match_info().get("name").unwrap_or("World");
-            format!("Hello, {}!", &name)
-        }
-        let __resource = ::actix_web::Resource::new("/")
-            .name("greet")
-            .guard(::actix_web::guard::Get())
-            .to(greet);
-        ::actix_web::dev::HttpServiceFactory::register(__resource, __config);
-    }
+// pub struct greet;
+// impl ::actix_web::dev::HttpServiceFactory for greet {
+//     fn register(self, __config: &mut actix_web::dev::AppService) {
+//         async fn greet(req: HttpRequest) -> impl Responder {
+//             let name = req.match_info().get("name").unwrap_or("World");
+//             format!("Hello, {}!", &name)
+//         }
+//         let __resource = ::actix_web::Resource::new("/")
+//             .name("greet")
+//             .guard(::actix_web::guard::Get())
+//             .to(greet);
+//         ::actix_web::dev::HttpServiceFactory::register(__resource, __config);
+//     }
+// }
+
+#[handler("/")]
+async fn greet(req: HttpRequest) -> impl Responder {
+     let name = req.match_info().get("name").unwrap_or("World");
+     format!("Hello, {}!", &name)
 }
 
 #[actix_web::main]
